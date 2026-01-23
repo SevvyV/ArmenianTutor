fa7f93c3c62be7d5ae89613fb0f37d38baddd6af
SevvyV <sevagshop@gmail.com>
2026-01-23 00:31:10 -0500

    Stable Version 5.18 Backup Jan23_2026

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#  Stable version 5.18 January 23rd, 2026
 import streamlit as st
 import time
 
@@ -267,3 +268,4 @@ elif "Lesson" in module:
     raw_data, prefix = lesson_map[module]
     st.header(f"📖 {module}")
     render_maximized_grid(raw_data, prefix)
+
